ea756476e85b393594c690551b71eb483b665156
uziramirez <124324345+uziramirez@users.noreply.github.com>
2026-04-28 18:13:31 -0500

    Updated package-lock.json

--- project/dist/app.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-npx electron .
-pause
--- project/dist/start.ps1
+++
@@ -1,4 +0,0 @@
-$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-Set-Location $scriptPath
-& npm install
-& npx electron .
